c06d3da6053ba000012e9901be1b00b83d9b8602
fatihahrhm <47657213+fatihahrhm@users.noreply.github.com>
2021-05-10 08:55:55 +0700

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from datetime import date, timedelta
+import re, string, sqlite3, tweepy
+
+def crawling(keyword):
+    consumer_key = "h5g9VvA0TxfSFnru8fzSAQif4"
+    consumer_secret = "iIovoGF3J35LwgJ7Ht6SBhvWiDYdN8xn9SuVRvE4Z5vsZZYXCt"
+    access_token = "1067941386-2IigAD1hKedjeNLS3DKsInbdnZ33rutJwlWYTYk"
+    access_token_secret = "tEiKbY4HbS58g72EOcCnuVk09Th29qOSt5gT1FQlow9IE"
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    search_words = keyword
+    new_search = search_words + " -filter:retweets"
+
+    tweets = tweepy.Cursor(api.search,
+                q=new_search,
+                lang="id", 
+                tweet_mode='extended').items(2000)    
+
+    items = []
+    tweetId = []
+    dates = []
+    username = []
+    for tweet in tweets:
+        items.append(tweet.full_text)
+        tweetId.append(tweet.id)
+        dates.append(tweet.created_at)
+        username.append(tweet.user.screen_name)
+
+    hasil = pd.DataFrame(list(zip(tweetId, dates, username, items)), columns=['tweetId','date','username','tweet'])
+
+    return hasil
+ 
+def save_to_csv(data, keyword):
+    file_name = keyword + " " + str(date.today()) + ".csv"
+    data.to_csv(file_name, index=False)
+
+keyword = "larangan mudik"
+tweets = crawling(keyword)
+save_to_csv(tweets, keyword)
+print(tweets)
